501aca2e759c2715228e955fc16963bfe8db8b83
nicolasconejeros-alt <nicolasconejerosotarola@gmail.com>
2026-07-01 15:57:49 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.title("Titulo del proyecto")
-st.image("https://www.freepik.es/fotos-vectores-gratis/computador/20")
+st.image("https://img.magnific.com/vector-premium/configuracion-moderna-computadoras-escritorio-productividad-creativa_1089479-30424.jpg?semt=ais_hybrid&w=740&q=80")
+st.write("Hecho por Nicolás Conejeros")
