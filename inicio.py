6bc39f74615a9a2ad00b68e308d4b8d36c545f13
nicolasconejeros-alt <nicolasconejerosotarola@gmail.com>
2026-07-01 16:08:37 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("Titulo del proyecto")
 st.image("https://img.magnific.com/vector-premium/configuracion-moderna-computadoras-escritorio-productividad-creativa_1089479-30424.jpg?semt=ais_hybrid&w=740&q=80")
 st.write("Hecho por Nicolás Conejeros")
+
+df = pd.read_csv('pokemon.csv')
+st.write(df)
