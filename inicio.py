58bd5cd1f6a9e58c4b5499ec6949657baea9f8ca
nicolasconejeros-alt <nicolasconejerosotarola@gmail.com>
2026-07-01 16:11:13 -0400

    Add files via upload

--- inicio.py
+++ inicio.py
@@ -5,5 +5,5 @@ st.title("Titulo del proyecto")
 st.image("https://img.magnific.com/vector-premium/configuracion-moderna-computadoras-escritorio-productividad-creativa_1089479-30424.jpg?semt=ais_hybrid&w=740&q=80")
 st.write("Hecho por Nicolás Conejeros")
 
-df = pd.read_csv('pokemon.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/nicolasconejeros-alt/Trabajo-Final-Programacion/refs/heads/main/pokemon.csv')
 st.write(df)
